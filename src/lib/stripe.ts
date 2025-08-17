0a3283946fad3ea4691b533f77cb37d69d4526dc
Zackary DeMori <jobblixor@gmail.com>
2025-08-16 23:43:34 -0400

    Update lib stribe file

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,3 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-});
-
-export default stripe;
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
